2678a3489fb34b1803a69428c3452370ca660cde
zhaoyuchen <yuchen.zhao@gaea.com>
2021-10-09 14:17:32 +0800

    提交责任链demo

---
+++ components/chainofresponsibilitypattern/src/main/java/chainpattern/ErrorLogger.java
@@ -0,0 +1,13 @@
+package chainpattern;
+
+public class ErrorLogger extends AbstractLogger {
+
+    public ErrorLogger(int level) {
+        this.level = level;
+    }
+
+    @Override
+    protected void write(String message) {
+        System.out.println("Standard Error::Logger: "+message);
+    }
+}
